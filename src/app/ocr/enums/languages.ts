d82a61630f18a5c826b1dc9e72bd954f843180ce
Hiyuu <tatsu.hiyuu@gmail.com>
2023-10-18 01:42:01 +0200

    trying to capture while importing

--- src/app/ocr/enums/languages.ts
+++ src/app/ocr/enums/languages.ts
@@ -1,3 +1,3 @@
 export enum Languages {
-    eng = 'English'
+    english = 'English'
 }
--- src/app/ocr/interfaces/language.ts
+++ src/app/ocr/interfaces/language.ts
@@ -3,3 +3,21 @@ export interface Language {
     code: string;
     language: string;
 }
+
+export interface English extends Language {
+    alpha2code: 'gb';
+    code: 'eng';
+    language: 'English';
+}
+
+export interface French extends Language {
+    alpha2code: 'fr';
+    code: 'fra';
+    language: 'French';
+}
+
+export interface Norwegian extends Language {
+    alpha2code: 'no';
+    code: 'nor';
+    language: 'Norwegian';
+}
