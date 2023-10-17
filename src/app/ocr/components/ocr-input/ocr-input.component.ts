6da24c12373efcfcfa71e17d7378f5394b3e6786
Hiyuu <tatsu.hiyuu@gmail.com>
2023-10-18 01:53:40 +0200

    Added condition to camera availability

--- src/app/ocr/components/ocr-input/ocr-input.component.ts
+++ src/app/ocr/components/ocr-input/ocr-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Language } from '../../interfaces/language';
 
 @Component({
@@ -6,11 +6,16 @@ import { Language } from '../../interfaces/language';
   templateUrl: './ocr-input.component.html',
   styleUrls: ['./ocr-input.component.css']
 })
-export class OcrInputComponent {
+export class OcrInputComponent implements OnInit {
   @ViewChild('fileSelector') fileSelector: ElementRef | undefined;
   @Output() uploadedImage: EventEmitter<File> = new EventEmitter<File>();
   @Output() languageSelected: EventEmitter<Language> = new EventEmitter<Language>();
   selectedLanguage: Language | undefined;
+  isCameraAvailable: boolean = false;
+
+  async ngOnInit(): Promise<void> {
+    await this.checkCameraAvailability();
+  }
 
   uploadFile(event: Event) {
     const element = event.currentTarget as HTMLInputElement;
@@ -28,5 +33,12 @@ export class OcrInputComponent {
   onLanguageSelected(language: Language): void {
     this.selectedLanguage = language;
     this.languageSelected.emit(language);
+  }
+
+  private async checkCameraAvailability(): Promise<void> {
+    await navigator.mediaDevices.getUserMedia({ video: true }).then((x) => {
+      console.log('x', x)
+      this.isCameraAvailable = true;
+    });
   }
 }
